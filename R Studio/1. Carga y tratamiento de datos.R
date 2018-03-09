0759feebb5647b591b0fae5bcba0ded58eb56948
Lorena <lorena_rc89@hotmail.com>
2018-03-09 13:30:22 +0100

    Definitivo

---
+++ R Studio/1. Carga y tratamiento de datos.R
@@ -0,0 +1,14 @@
+# Para todo el trabajo en R, obtenemos la ruta  y fijamos nuestro directorio de trabajo.
+getwd()
+setwd("C:/Users/satellite/Final-Project-MDS/R Studio")
+
+
+# En primer lugar cargamos el dataset, lo cargamos desde la web directamente de la siguiente manera:
+who1 <- read.csv('https://query.data.world/s/8Pb1O_ASzuDGfaFfGu6w6kLT6t6IwL', header=TRUE,stringsAsFactors = FALSE)
+
+# Podemos cargarlo también desde la ruta donde hemos guardado el fichero.
+who1 <- read.csv('WHO.csv', dec=",",sep=",", header=T, stringsAsFactors = FALSE)
+
+# Visualizamos el contenido del dataset y hacemos un summary para tener una idea general de los datos.
+View(who1)
+summary(who1)
