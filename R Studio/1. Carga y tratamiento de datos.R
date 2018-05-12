ae78a5dcf1123761629762b83f2dfe1a59a05afc
Lorena <lorena_rc89@hotmail.com>
2018-05-12 21:31:38 +0200

    Trabajo 12Mayo

--- R Studio/1. Carga y tratamiento de datos.R
+++ R Studio/1. Carga y tratamiento de datos.R
@@ -7,8 +7,8 @@ setwd("C:/Users/satellite/Final-Project-MDS/R Studio")
 who1 <- read.csv('https://query.data.world/s/8Pb1O_ASzuDGfaFfGu6w6kLT6t6IwL', header=TRUE,stringsAsFactors = FALSE)
 
 # Podemos cargarlo también desde la ruta donde hemos guardado el fichero.
-who1 <- read.csv('WHO.csv', dec=",",sep=",", header=T, stringsAsFactors = FALSE)
+who1 <- read.csv('WHO.csv', dec=".",sep=",", header=T, stringsAsFactors = FALSE)
 
-# Visualizamos el contenido del dataset y hacemos un summary para tener una idea general de los datos.
-View(who1)
+# Visualizamos las columnas que tenemos y hacemos un summary para tener una idea general de los datos.
+colnames(who1)
 summary(who1)
